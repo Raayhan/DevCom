67570e8d27eb5bb5634e7c05b37cac4cf0fd82f8
Raayhan <rayhan.rakib@northsouth.edu>
2022-02-01 20:48:05 +0600

    Added Model for Room

--- base/models.py
+++ base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    
+    #host       =
+    #topic      =
+    name        = models.CharField(max_Length=200)
+    description = models.textField(null=True,blank=True)
+    #participants =
+    updated     = models.dateTimeField(auto_now=True)
+    created     = models.dateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.name
+    
