b036fa15899030f0781cb1a9ff7386ce6fd2caf4
Raayhan <rayhan.rakib@northsouth.edu>
2022-02-01 21:56:12 +0600

    Updated Views for Room

--- base/views.py
+++ base/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
 from .models import Room
-rooms = [
-    {'id':1, 'name':'Python Crash Course'},
-    {'id':2, 'name':'React Font to Back'},
-    {'id':3, 'name':'Laravel From Scratch'},
-]
 
 def home(request):
+    rooms   = Room.objects.all()
     context= {'rooms':rooms}
     return render (request,'base/home.html',context)
 
 def room(request,pk):
 
-    room=None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room=i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render (request,'base/room.html',context)
 
